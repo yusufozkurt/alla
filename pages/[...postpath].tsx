c7ba094e15097d71d5686b8dedf001911c41b755
kathrynb fierro <134311762+yusufozkurt@users.noreply.github.com>
2023-08-18 17:55:54 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -84,7 +84,8 @@ const Post: React.FC<PostProps> = (props) => {
 			<Head>
 				
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+			<!--
+					<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
@@ -94,7 +95,7 @@ const Post: React.FC<PostProps> = (props) => {
 				<!--<meta property="og:image:width" content="1024">
 				<meta property="og:image:height" content="614">
 				<meta property="og:image:type" content="image/png">
-					-->
+		-->
 				
 				
 				<meta
