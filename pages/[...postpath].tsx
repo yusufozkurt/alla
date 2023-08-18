a7ff9d2e23323c3987607d573ccac84608d62b8d
kathrynb fierro <134311762+yusufozkurt@users.noreply.github.com>
2023-08-18 17:48:13 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -104,7 +104,7 @@ const Post: React.FC<PostProps> = (props) => {
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
+				
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.altText || post.title}
